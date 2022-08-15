356d81cf569e82ca2d48e64b61317bb33ca1cf1d
SoA-Lee <llsa0927@naver.com>
2022-08-15 18:25:22 +0900

    algorithm

---
+++ Algorithm/05_Devide&Conquer(분할정복)/Devide&Conquer01.java
@@ -0,0 +1,24 @@
+// 알고리즘 - 분할정복
+// 재귀호출 구조 구현
+
+public class Main {
+
+    public static int getMax(int[] arr, int left, int right) {
+        int m = (left + right) / 2;
+
+        if (left == right) {
+            return arr[left];
+        }
+
+        left = getMax(arr, left, m);
+        right = getMax(arr, m + 1, right);
+
+        return (left > right) ? left : right;
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {3, 5, 10, 50, 25, 30, 1, 15};
+        System.out.println(getMax(arr, 0, arr.length - 1));
+    }
+}
